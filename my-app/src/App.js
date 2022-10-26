278478606eeca12b0860e83e0b20e4753b6683cc
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-10-25 22:17:34 -0700

    Added mui depedencies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
+import * as React from 'react';
 import { createMessageComponent } from './createMessageComponent.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* {createMessageComponent()} */}
-        
+      <header className="App-header">        
       </header>
+      {createMessageComponent()}
+
     </div>
   );
 }
--- my-app/src/createMessageComponent.js
+++ my-app/src/createMessageComponent.js
@@ -1,13 +1,19 @@
-
+import * as React from 'react';
+import TextareaAutosize from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 export function createMessageComponent() {
-    return 
-    <div>
-        <TextareaAutosize
-            aria-label="empty textarea"
-            placeholder="Empty"
-            style={{ width: 200 }}>
-                Hello mom
-        </TextareaAutosize>
-    </div>
+    return (
+        <div>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <TextareaAutosize
+                aria-label="empty textarea"
+                placeholder="Empty"
+                style={{ width: 200 }}>
+                    Hello mom
+            </TextareaAutosize>
+        </div>
+    )
 }
