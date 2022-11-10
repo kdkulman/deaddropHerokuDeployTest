77dca30e412374770ffeb6eb8455ad4214b62d62
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-11-10 12:02:36 -0800

    update

--- client/src/BackEndFunctionsSQL/testFetch.js
+++ client/src/BackEndFunctionsSQL/testFetch.js
@@ -1,16 +1,5 @@
-export async function fetchText() {
-    let response = await 
-    fetch("https://dead-drop-app-web-service.herokuapp.com/")
-        .then((res) => res.json())
-        .then((json) => {
-            this.setState({
-                items: json,
-                DataisLoaded: true
-            });
-        }
-    );
-
-    let data = await response.text();
-    console.log(data);
+export function fetchText() {
+    fetch('https://dead-drop-app-web-service.herokuapp.com/')
+        .then((response) => response.json())
+            .then((data) => console.log(data));
 }
-
