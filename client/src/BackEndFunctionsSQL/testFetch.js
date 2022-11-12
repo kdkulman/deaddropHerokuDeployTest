6c4c5d79aa320998fbcab90091782a40678f32d0
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-11-12 14:02:41 -0800

    refactor

--- client/src/BackEndFunctionsSQL/testFetch.js
+++ client/src/BackEndFunctionsSQL/testFetch.js
@@ -1,13 +1,8 @@
-export async function fetchText() {
-    let response = await fetch('http://dead-drop-app-web-service.herokuapp.com/')
-    let responseJSON = await response.json();
-    let data = JSON.parse(responseJSON);
-    console.log(data);
-        // .then(response => {
-        //     let data = response.text();
-        //     console.log(data);
-        // })
-        // .catch(error => {
-        //     // handle the error
-        // });
+export function fetchText() {
+    fetch(`http://localhost:5000/`)
+    .then((result) => result.text())
+    .then((data) => {
+        console.log(data);
+    })
+
 }
